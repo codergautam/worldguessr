d2c255e1f2ef9814cd7a0bfbb64a0e805573ce07
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2024-04-30 12:15:40 -0500

    Update country.js

--- pages/api/country.js
+++ pages/api/country.js
@@ -6,7 +6,7 @@
    const { lat, long } = req.query;
   if(!lat || !long) return res.status(400).json({address: {country: null}});
    const output = lookup(parseFloat(lat), parseFloat(long), true);
-  if(output && output.length > 0)
+  if(output && output.length > 0) {
           res.status(200).json({address: {country: output[0]}});
  } else {
   res.status(200).json({address: {country: null}});
