9fe35e38605cef34e84b9c26ea588e384e0c7f61
Gautam Anand <gautamgxtv@gmail.com>
2024-04-30 20:06:46 -0500

    Fix clicking logic

--- components/Map.js
+++ components/Map.js
@@ -13,11 +13,62 @@ import { Icon, Style, Stroke } from 'ol/style';
 import { fromLonLat, toLonLat, transformExtent } from 'ol/proj';
 import { getDistance } from 'ol/sphere';
 import ol from 'ol/interaction';
+import { Circle } from 'ol/geom';
 
 const MapComponent = ({ pinPoint, setPinPoint, guessed, location, setKm, height, guessing, multiplayerSentGuess, playingMultiplayer, multiplayerGameData, currentId, round }) => {
   const mapRef = useRef();
   const [map, setMap] = useState(null);
   const vectorSource = useRef(new VectorSource());
+
+  function drawHint(initialMap, location) {
+      // create a circle overlay 100km radius from location
+      // const circleRadius = 10000000;
+      // const circleX = location.long + (Math.random() - 0.5) * circleRadius;
+      // const circleY = location.lat + (Math.random() - 0.5) * circleRadius;
+      // const circleFeature = new Feature({
+      //   geometry: new Circle(fromLonLat([circleX, circleY]), circleRadius),
+      // });
+      // circleFeature.setStyle(
+      //   new Style({
+      //     renderer(coordinates, state) {
+      //       const [[x, y], [x1, y1]] = coordinates;
+      //       const ctx = state.context;
+      //       const dx = x1 - x;
+      //       const dy = y1 - y;
+      //       const radius = Math.sqrt(dx * dx + dy * dy);
+
+      //       const innerRadius = 0;
+      //       const outerRadius = radius * 1.4;
+
+      //       const gradient = ctx.createRadialGradient(
+      //         x,
+      //         y,
+      //         innerRadius,
+      //         x,
+      //         y,
+      //         outerRadius,
+      //       );
+      //       gradient.addColorStop(0, 'rgba(255,0,0,0)');
+      //       gradient.addColorStop(0.6, 'rgba(255,0,0,0.2)');
+      //       gradient.addColorStop(1, 'rgba(255,0,0,0.8)');
+      //       ctx.beginPath();
+      //       ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
+      //       ctx.fillStyle = gradient;
+      //       ctx.fill();
+
+      //       ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
+      //       ctx.strokeStyle = 'rgba(255,0,0,1)';
+      //       ctx.stroke();
+      //     },
+      //   }),
+      // );
+      // const circleLayer = new VectorLayer({
+      //   source: new VectorSource({
+      //     features: [circleFeature]
+      //   }),
+      // });
+      // initialMap.addLayer(circleLayer);
+  }
   // Initialize map on first render
   useEffect(() => {
     const initialMap = new Map({
@@ -39,22 +90,37 @@ const MapComponent = ({ pinPoint, setPinPoint, guessed, location, setKm, height,
       }),
     });
 
-    const mouseDown = (e) => {
+    if(location) drawHint(initialMap, location);
+
+
+    // const mouseDown = (e) => {
+    //   if (!guessed && !guessing) {
+    //     e.preventDefault();
+    //     const pixel = initialMap.getEventPixel(e);
+    //     const clickedCoord = initialMap.getCoordinateFromPixel(pixel);
+    //     const clickedLatLong = toLonLat(clickedCoord);
+    //     setPinPoint({ lat: clickedLatLong[1], lng: clickedLatLong[0] });
+    //   }
+    // };
+    // mapRef.current.addEventListener('mousedown', mouseDown);
+
+    // use map click event to set pin point
+    function onMapClick(e) {
       if (!guessed && !guessing) {
-        e.preventDefault();
-        const pixel = initialMap.getEventPixel(e);
-        const clickedCoord = initialMap.getCoordinateFromPixel(pixel);
+        const clickedCoord = initialMap.getEventCoordinate(e.originalEvent);
         const clickedLatLong = toLonLat(clickedCoord);
         setPinPoint({ lat: clickedLatLong[1], lng: clickedLatLong[0] });
       }
-    };
-    mapRef.current.addEventListener('mousedown', mouseDown);
+    }
+    initialMap.on('click', onMapClick);
 
     setMap(initialMap);
 
     return () => {
       initialMap.setTarget(undefined);
-      if(mapRef.current) mapRef.current.removeEventListener('mousedown', mouseDown);
+      // if(mapRef.current) mapRef.current.removeEventListener('mousedown', mouseDown);
+
+        initialMap.un('click', onMapClick);
     };
   }, [guessed, setPinPoint, guessing]);
 
@@ -63,7 +129,7 @@ const MapComponent = ({ pinPoint, setPinPoint, guessed, location, setKm, height,
     if (!map) return;
 
     vectorSource.current.clear();
-
+    if(location) drawHint(map, location);
     if (pinPoint) {
       const pinFeature = new Feature({
         geometry: new Point(fromLonLat([pinPoint.lng, pinPoint.lat])),
