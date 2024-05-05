ebd64c9f86f2a4022cd771e2e2cc92c90f96b978
Gautam Anand <gautamgxtv@gmail.com>
2024-05-04 21:52:26 -0500

    Update loader.js to add alt attribute to image tag

--- components/loader.js
+++ components/loader.js
@@ -1,7 +1,7 @@
 export default function Loader({latLong, loading, loadingText}) {
   return (
     <div style={{position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)", opacity: (!latLong || loading) ? '1' : '0', transition: 'all 250ms ease'}}>
-<img  src="/load.gif" />
+<img  src="/load.gif" alt="loading"/>
 <h1 style={{color: 'white', fontSize: '24px', marginTop: '20px', textAlign: 'center'}}>{loadingText || 'Loading...'}</h1>
 </div>
   )
