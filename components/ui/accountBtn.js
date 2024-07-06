2d0d68dfe5b5467ccfc2375f464ee663481f6fd7
max woollons <maxwoollons@outlook.com>
2024-07-06 21:22:14 +1000

    Fixed up top right corner button padding issues

--- components/ui/accountBtn.js
+++ components/ui/accountBtn.js
@@ -12,15 +12,16 @@ export default function AccountBtn({ session, openAccountModal, navbarMode }) {
 
         { !session?.token?.secret && session !== null ? '...' :
         (
-          <>
-          {text("login")}
-        <FaGoogle className="home__squarebtnicon" />
-          </>
+          <div style={{marginRight: '10px',marginLeft: '10px'}}>
+            
+            {text("Login",{lng: "en"})}
+            <FaGoogle className="home__squarebtnicon" />
+          </div> 
         )}
         </button>
     ) : (
         <button className={`gameBtn ${navbarMode ? 'navBtn' : 'accountBtn'}`} onClick={() => openAccountModal()}>
-          {session?.token?.username ? <p style={{ color: 'white', marginRight: '10px' }}>{session?.token?.username}</p> : null}
+          {session?.token?.username ? <p style={{ color: 'white', marginRight: '10px',marginLeft: '10px' }}>{session?.token?.username}</p> : null}
         </button>
     )}
     </>
