632da7ce74731d522ca22431ca1b68b3e74964dc
Gautam Anand <gautamgxtv@gmail.com>
2024-08-12 20:13:10 -0500

    iOS fixes

--- components/utils/inIframe.js
+++ components/utils/inIframe.js
@@ -1,3 +1,6 @@
+const forbiddenIframe = [
+  "https://openguessr.io/" // request from openguessr.com owner to prevent embedding on this unauthorized website
+];
 export function inIframe() {
   try {
       return window.self !== window.top;
