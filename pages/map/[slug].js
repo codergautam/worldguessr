f71dd5470f03a68e701158a05eda93285f30d559
Gautam Anand <gautamgxtv@gmail.com>
2024-08-17 09:12:20 -0500

    Improve map page

--- pages/map/[slug].js
+++ pages/map/[slug].js
@@ -14,6 +14,7 @@ import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import officialCountryMaps from '@/public/officialCountryMaps.json';
 import { FaInfinity } from 'react-icons/fa6';
+import { getSession } from 'next-auth/react';
 
 export async function getServerSideProps(context) {
   const { slug } = context.params;
@@ -27,6 +28,8 @@ export async function getServerSideProps(context) {
           description_short: cntryMap.shortDescription,
           description_long: cntryMap.longDescription,
           created_by: "WorldGuessr",
+          in_review: false,
+          rejected: false
         },
         ...(await serverSideTranslations(locale, [
           'common',
@@ -34,6 +37,9 @@ export async function getServerSideProps(context) {
       }
     };
   }
+
+  const session = await getSession(context);
+  const staff = session?.token?.staff;
 
   const map = await Map.findOne({ slug }).lean();
   if (!map) {
@@ -44,10 +50,20 @@ export async function getServerSideProps(context) {
   }
 
   const authorId = map.created_by;
-  const author = await User.findOne({ _id: authorId }).lean();
-  map.created_by = author.username;
-  map.created_at = msToTime(Date.now() - map.created_at);
+  const authorUser = await User.findById(authorId).lean();
+  const authorSecret = authorUser?.secret;
 
+
+  const isCreatorOrStaff = session && (authorSecret === session?.token?.secret || staff);
+
+  if (!map.accepted && !isCreatorOrStaff) {
+    return {
+      notFound: true,
+    };
+  }
+
+  map.created_by = authorUser?.username;
+  map.created_at = msToTime(Date.now() - map.created_at);
 
   return {
     props: {
@@ -64,64 +80,68 @@ export default function MapPage({ mapData }) {
   const [currentLocationIndex, setCurrentLocationIndex] = useState(0);
   const [locationUrls, setLocationUrls] = useState([]);
   const [fadeClass, setFadeClass] = useState(styles.iframe);
-  const { t: text} = useTranslation('common');
+  const { t: text } = useTranslation('common');
 
   useEffect(() => {
-    if(!mapData.data) return;
-    // Generate the URLs for the iframe based on the map data
+    if (!mapData.data) return;
+
     const urls = mapData.data.map(location =>
       `//www.google.com/maps/embed/v1/streetview?key=AIzaSyA2fHNuyc768n9ZJLTrfbkWLNK3sLOK-iQ&location=${location.lat},${location.lng}&fov=60`
     );
     setLocationUrls(urls);
 
-    // Cycle through the locations every 5 seconds
     const intervalId = setInterval(() => {
       setFadeClass(styles.iframe + ' ' + styles.fadeOut);
       setTimeout(() => {
-        // setCurrentLocationIndex(prevIndex => (prevIndex + 1) % urls.length);
-        // random locations
         setCurrentLocationIndex(Math.floor(Math.random() * urls.length));
         setFadeClass(styles.iframe + ' ' + styles.fadeIn);
-      }, 1000); // Match with the CSS transition duration
-
+      }, 1000);
     }, 5000);
 
-    // Cleanup interval on component unmount
     return () => clearInterval(intervalId);
   }, [mapData.data]);
 
   const handlePlayButtonClick = () => {
-    // Placeholder function for play button
-    // router.push(`/?map=${mapData.slug}`);
-    window.location.href = `/?map=${mapData.countryCode || mapData.slug}`; // Redirect to game page
+    window.location.href = `/?map=${mapData.countryCode || mapData.slug}`;
   };
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>{mapData.name+" - Play Free on WorldGuessr"}</title>
+        <title>{mapData.name + " - Play Free on WorldGuessr"}</title>
         <meta name="description" content={`Explore ${mapData.name} on WorldGuessr, a free GeoGuessr clone. ${mapData.description_short}`} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <style>
-      {`
-              .mainBody {
+      <style>
+        {`
+          .mainBody {
             user-select: auto !important;
             overflow: auto !important;
-        }
+          }
         `}
-    </style>
+      </style>
       <main className={styles.main}>
         <Navbar />
+
+        {mapData.in_review && (
+          <div className={styles.statusMessage}>
+            <p>⏳ This map is currently under review.</p>
+          </div>
+        )}
+
+        {mapData.reject_reason && (
+          <div className={styles.statusMessage}>
+            <p>❌ This map has been rejected: {mapData.reject_reason}</p>
+          </div>
+        )}
 
         <div className={styles.branding}>
           <h1>WorldGuessr</h1>
           <p>{text('freeGeoguessrAlt')}</p>
-          {/* Back to Game */}
           <center>
-          <button onClick={()=>window.location.href="/"} className={styles.backButton}>
-            ← {text('backToGame')}
-          </button>
+            <button onClick={() => window.location.href="/"} className={styles.backButton}>
+              ← {text('backToGame')}
+            </button>
           </center>
         </div>
 
@@ -140,7 +160,7 @@ export default function MapPage({ mapData }) {
             )}
 
             {mapData.countryCode && (
-              <img src={`https://flagcdn.com/w2560/${mapData.countryCode.toLowerCase()}.png`} style={{width: "100%", height: "100%", objectFit: "cover"}} />
+              <img src={`https://flagcdn.com/w2560/${mapData.countryCode.toLowerCase()}.png`} style={{ width: "100%", height: "100%", objectFit: "cover" }} />
             )}
           </div>
           <div className={styles.mapInfo}>
@@ -150,25 +170,25 @@ export default function MapPage({ mapData }) {
         </div>
 
         <div className={styles.mapStats}>
-          { typeof mapData.plays !== "undefined" && (
-          <div className={styles.stat}>
-            <span className={styles.statIcon}>👥</span>
-            <span className={styles.statValue}>{mapData.plays.toLocaleString()}</span>
-            <span className={styles.statLabel}>Plays</span>
-          </div>
+          {typeof mapData.plays !== "undefined" && (
+            <div className={styles.stat}>
+              <span className={styles.statIcon}>👥</span>
+              <span className={styles.statValue}>{mapData.plays.toLocaleString()}</span>
+              <span className={styles.statLabel}>Plays</span>
+            </div>
           )}
 
           <div className={styles.stat}>
             <span className={styles.statIcon}>📍</span>
-            <span className={styles.statValue}>{mapData.data ? formatNumber(mapData.data.length, 3) : <FaInfinity /> }</span>
+            <span className={styles.statValue}>{mapData.data ? formatNumber(mapData.data.length, 3) : <FaInfinity />}</span>
             <span className={styles.statLabel}>Locations</span>
           </div>
-          { typeof mapData.hearts !== "undefined" && (
-          <div className={styles.stat}>
-            <span className={styles.statIcon}>❤️</span>
-            <span className={styles.statValue}>{mapData.hearts.toLocaleString()}</span>
-            <span className={styles.statLabel}>Hearts</span>
-          </div>
+          {typeof mapData.hearts !== "undefined" && (
+            <div className={styles.stat}>
+              <span className={styles.statIcon}>❤️</span>
+              <span className={styles.statValue}>{mapData.hearts.toLocaleString()}</span>
+              <span className={styles.statLabel}>Hearts</span>
+            </div>
           )}
         </div>
 
@@ -178,14 +198,13 @@ export default function MapPage({ mapData }) {
 
         <div className={styles.mapDescription}>
           <h2>About this map</h2>
-          { mapData.description_long.split('\n').map((line, index) => <p key={index}>{line}</p>) }
+          {mapData.description_long.split('\n').map((line, index) => <p key={index}>{line}</p>)}
           <p className={styles.mapAuthor}>
             Created by <strong>{mapData.created_by}</strong>
-
             {mapData.created_at && (
-            `${mapData.created_at} ago`
+              ` ${mapData.created_at} ago`
             )}
-            </p>
+          </p>
         </div>
       </main>
     </div>
