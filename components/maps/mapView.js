f29d35001751dd8baa8f7529485798e1468f9a79
Gautam Anand <gautamgxtv@gmail.com>
2024-09-19 22:36:23 -0500

    fgh

--- components/maps/mapView.js
+++ components/maps/mapView.js
@@ -123,7 +123,7 @@ export default function MapView({ gameOptions, setGameOptions, singleplayer, clo
         try {
           json = await res.json();
         } catch (e) {
-          toast.error("Unexpected Error - 1");
+          toast.error("Unexpected Error - 1 (This can happen if your map file is too big, please contact us for a manual upload)");
           setMakeMap({ ...makeMap, progress: false });
           return;
         }
