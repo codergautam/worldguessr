56bb994e78843df38a9004d8df47aa02fa2097f9
Gautam Anand <gautamgxtv@gmail.com>
2024-10-15 07:26:47 -0500

    fgh

--- server.js
+++ server.js
@@ -1016,6 +1016,7 @@ Key is saved at:         /etc/letsencrypt/live/www.worldguessr.com/privkey.pem
       }
       if (json.type === 'verify') {
         // account verification
+        console.log(json.secret);
         if((!json.secret) ||(json.secret === 'not_logged_in')) {
           if(!player.verified) {
 
