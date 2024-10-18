caac9a3396aaeffdfbaaf313246cbf426faf09ec
Gautam Anand <gautamgxtv@gmail.com>
2024-10-18 12:25:15 -0500

    Test

--- components/utils/localStorage.js
+++ components/utils/localStorage.js
@@ -5,6 +5,7 @@ export default class gameStorage {
   }
 
   static setItem(key, value) {
+    console.log('setItem', key, value, window.inCrazyGames );
     try {
       if(gameStorage.isCrazyGames()) {
         window.CrazyGames.SDK.data.setItem(key, value);
@@ -14,6 +15,7 @@ export default class gameStorage {
     } catch (e) {}
   }
   static getItem(key) {
+    console.log('getItem', key, window.inCrazyGames );
     try {
       if(gameStorage.isCrazyGames()) {
         return window.CrazyGames.SDK.data.getItem(key);
@@ -23,6 +25,7 @@ export default class gameStorage {
     } catch (e) {}
   }
   static removeItem(key) {
+    console.log('removeItem', key, window.inCrazyGames );
     try {
       if(gameStorage.isCrazyGames()) {
         window.CrazyGames.SDK.data.removeItem(key);
