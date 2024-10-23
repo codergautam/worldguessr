700e6ee877cfd70ab6cc97a86c2198ba14cf7fb6
Gautam Anand <gautamgxtv@gmail.com>
2024-10-22 23:59:14 -0500

    Add discord link

--- components/home.js
+++ components/home.js
@@ -1745,9 +1745,10 @@ setShowCountryButtons(false)
           <div className="footer_btns">
         { !isApp && (
                   <>
+                <Link target="_blank" href={"https://discord.gg/ubdJHjKtrC"}><button className="home__squarebtn gameBtn discord" aria-label="Discord"><FaDiscord className="home__squarebtnicon" /></button></Link>
+
                   { !inCrazyGames && (
                     <>
-                <Link target="_blank" href={"https://discord.gg/ubdJHjKtrC"}><button className="home__squarebtn gameBtn discord" aria-label="Discord"><FaDiscord className="home__squarebtnicon" /></button></Link>
                 <Link target="_blank" href={"https://www.youtube.com/@worldguessr?sub_confirmation=1"}><button className="home__squarebtn gameBtn youtube" aria-label="Youtube"><FaYoutube className="home__squarebtnicon" /></button></Link>
                 <Link target="_blank" href={"https://github.com/codergautam/worldguessr"}><button className="home__squarebtn gameBtn" aria-label="Github"><FaGithub className="home__squarebtnicon" /></button></Link>
                 </>
