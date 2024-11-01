98ead83190d29b1753f587180fd9c6f1f13b7f3f
Gautam Anand <gautamgxtv@gmail.com>
2024-10-31 21:50:48 -0500

    Fix header limit bug

--- components/auth/auth.js
+++ components/auth/auth.js
@@ -12,6 +12,27 @@ export function signOut() {
   if(window.dontReconnect) {
     return;
   }
+
+  // remove all cookies
+  console.log("Removing cookies");
+  (function () {
+    var cookies = document.cookie.split("; ");
+    for (var c = 0; c < cookies.length; c++) {
+        var d = window.location.hostname.split(".");
+        while (d.length > 0) {
+            var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+            var p = location.pathname.split('/');
+            document.cookie = cookieBase + '/';
+            while (p.length > 0) {
+                document.cookie = cookieBase + p.join('/');
+                console.log(cookieBase + p.join('/'));
+                p.pop();
+            };
+            d.shift();
+        }
+    }
+})();
+
   window.location.reload();
 }
 
--- ws/ws.js
+++ ws/ws.js
@@ -17,10 +17,7 @@ import { players, games } from '../serverUtils/states.js';
 import Memsave from '../models/Memsave.js';
 import blockedAt from 'blocked-at';
 
-import axios from 'axios';
-
 config();
-
 // Load the profanity filter
 const filter = new Filter();
 filter.removeWords('damn')
@@ -198,7 +195,9 @@ process.on('unhandledRejection', (reason, promise) => {
   stop('unhandledRejection');
 });
 // uWebSockets.js
-let app = uws.App();
+let app = uws.App({
+
+});
 app.listen('0.0.0.0', port, (ws) => {
   if (ws) {
     log('**WS Server started on port** ' + port);
@@ -206,18 +205,21 @@ app.listen('0.0.0.0', port, (ws) => {
 });
 
 app.get('/', (res, req) => {
-  // make sureonly cf can access
-  let ip = req.getHeader('cf-connecting-ip') || res.getRemoteAddressAsText();
-  // cjeck if ip isarraybuffer
-  if (ip instanceof ArrayBuffer) {
-    ip = new TextDecoder().decode(ip);
-  }
+
+      // count all the headers
+      let headerKb = 0;
+      req.forEach((key, value) => {
+
+        headerKb += key.length + value.length;
+
+      });
+      headerKb = headerKb / 1024;
 
 
   setCorsHeaders(res);
-  res.writeHeader('Content-Type', 'text/plain');
+  res.writeHeader('Content-Type', 'text/html');
   res.writeStatus('200 OK');
-  res.end("WorldGuessr - Powered by uWebSockets.js");
+  res.end("WorldGuessr - Powered by uWebSockets.js<br>Headers: "+headerKb.toFixed(2)+'kb');
 });
 
 // maintenance mode
