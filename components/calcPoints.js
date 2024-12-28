2d80e515069ae081f5efd9fb71d46122426d0b0e
Gautam Anand <gautamgxtv@gmail.com>
2024-12-28 10:09:24 -0600

    Fix bug with latlong

--- components/calcPoints.js
+++ components/calcPoints.js
@@ -2,6 +2,7 @@ Number.prototype.toRad = function() {
   return this * Math.PI / 180;
 }
 export function findDistance(lat1, lon1, lat2, lon2) {
+  try {
   var R = 6371; // km
   var dLat = (lat2-lat1).toRad();
   var dLon = (lon2-lon1).toRad();
@@ -10,6 +11,10 @@ export function findDistance(lat1, lon1, lat2, lon2) {
           Math.sin(dLon/2) * Math.sin(dLon/2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
   var d = R * c;
+  } catch(e) {
+    console.log(e);
+    return 0;
+  }
   return d;
 }
 export default function calcPoints({lat, lon, guessLat, guessLon, usedHint, maxDist}) {
