02d50414547d27b011f3e3d6e925e80d01713746
Gautam Anand <gautamgxtv@gmail.com>
2025-03-16 22:52:29 -0500

    Display SSP on debug ad serving

--- components/bannerAdAdinplay.js
+++ components/bannerAdAdinplay.js
@@ -199,7 +199,7 @@ export default function Ad({
                 backgroundColor: "rgba(0, 0, 0, 0.5)",
               }}
             >
-              <h3>Banner Ad Here</h3>
+              <h3>Banner Ad Here (Adinplay)</h3>
               <p style={{ fontSize: "0.8em" }}>
                 Ad size: {types[type][0]} x {types[type][1]}
               </p>
