1dba40e13f25718b970177899413122a644aa809
Gautam Anand <gautamgxtv@gmail.com>
2025-05-21 20:31:37 -0500

    Restrict maintenance notice to larger screens

--- components/home.js
+++ components/home.js
@@ -2183,7 +2183,9 @@ if(inCrazyGames) {
               <>
             <h1 className="home__title">WorldGuessr</h1>
 
+              { height > 650 && width > 1000 && (
             <HomeNotice text={text("maintenanceText1", {date: "May 23rd", time: getTimeString()})} shown={true} />
+              )}
             </>
             )}
 
