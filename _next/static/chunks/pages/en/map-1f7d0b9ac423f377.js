54e969accc08e07d1f31870e47d31f9647bc2f8f
codergautam <codergautam@users.noreply.github.com>
2026-04-29 19:36:34 +0000

    CoolMath build from 03db55c7c138a3e759a11d2a51f6c7ddddf9ed43 03db55c7c138a3e759a11d2a51f6c7ddddf9ed43

---
+++ _next/static/chunks/pages/en/map-1f7d0b9ac423f377.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7710],{3388:(_,e,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/en/map",function(){return u(9589)}])},9589:(_,e,u)=>{"use strict";u.r(e),u.d(e,{default:()=>n.default});var n=u(7623)}},_=>{_.O(0,[4938,8585,4988,1173,1213,6194,636,6593,8792],()=>_(_.s=3388)),_N_E=_.O()}]);
