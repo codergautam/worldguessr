1a277448a4f81a2d7012f20b0d16948c775221e5
Gautam Anand <gautamgxtv@gmail.com>
2026-05-02 06:37:30 -0500

    Update package.json to add @capacitor/core and @capacitor/cli dependencies, ensuring compatibility with the latest features and improvements.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.codergautamyt.worldguessr',
+  appName: 'worldguessr',
+  webDir: 'public'
+};
+
+export default config;
